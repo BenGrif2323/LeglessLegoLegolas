0f15683c42aa2348e03219a2a9d21b03ca51459c
BenGrif2323 <83267311+BenGrif2323@users.noreply.github.com>
2023-03-02 12:06:09 -0800

    Modified script.js
    Can now read the text of a .ldr file

--- WebUI/scripts/script.js
+++ WebUI/scripts/script.js
@@ -1,71 +1,58 @@
-const input = document.querySelector('input');
+const input = document.getElementById('file');
 const preview = document.querySelector('.preview');
 
 input.style.opacity = 1;
-input.addEventListener('change', updateImageDisplay);
+//input.addEventListener('change', readLDR);
 
-function updateImageDisplay() {
-    while(preview.firstChild) {
-      preview.removeChild(preview.firstChild);
+document.querySelector('input')
+  .addEventListener('change', function () {
+
+    var fr = new FileReader();
+    fr.onload = function () {
+      document.querySelector('.output')
+        .textContent = fr.result;
     }
-  
-    const curFiles = input.files;
-    if (curFiles.length === 0) {
-      const para = document.createElement('p');
-      para.textContent = 'No files currently selected for upload';
-      preview.appendChild(para);
-    } else {
-      const list = document.createElement('ul');
-      preview.appendChild(list);
 
-      for (const file of curFiles) {
-        const listItem = document.createElement('li');
-        const para = document.createElement('p');
-        if (validFileType(file)) {
-          para.textContent = `File name ${file.name}, file size ${returnFileSize(file.size)}.`;
-          const image = document.createElement('img');
-          image.src = URL.createObjectURL(file);
-  
-          listItem.appendChild(image);
-          listItem.appendChild(para);
-        } else {
-          para.textContent = `File name ${file.name}: Not a valid file type. Update your selection.`;
-          listItem.appendChild(para);
-        }
-  
-        list.appendChild(listItem);
-      }
-    }
+    fr.readAsText(this.files[0]);
+  })
+
+function readLDR() {
+  while (preview.firstChild) {
+    preview.removeChild(preview.firstChild);
   }
 
-  // https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types
-const fileTypes = [
-    "image/apng",
-    "image/bmp",
-    "image/gif",
-    "image/jpeg",
-    "image/pjpeg",
-    "image/png",
-    "image/svg+xml",
-    "image/tiff",
-    "image/webp",
-    "image/x-icon"
-  ];
-
-  function addLegoLocation(file, list) {
-    
+  const curFiles = input.files;
+  if (curFiles.length === 0) {
+    const para = document.createElement('p');
+    para.textContent = 'No files currently selected';
+    preview.appendChild(para);
   }
-
-  function validFileType(file) {
-    return fileTypes.includes(file.type);
+  else if (curFiles.length > 1) {
+    const para = document.createElement('p');
+    para.textContent = 'More than one file selected';
+    preview.appendChild(para);
   }
+  else {
+    const list = document.createElement('ul');
+    preview.appendChild(list);
+    const file = curFiles.item(0);
+    let fileReader = new FileReader();
+    fileReader.readAsText(file);
+    const listItem = document.createElement('li');
+    const para = document.createElement('li');
+    para.textContent = fileReader.result;
+    list.appendChild(para);
+    list.appendChild(listItem);
 
-  function returnFileSize(number) {
-    if (number < 1024) {
-      return `${number} bytes`;
-    } else if (number >= 1024 && number < 1048576) {
-      return `${(number / 1024).toFixed(1)} KB`;
-    } else if (number >= 1048576) {
-      return `${(number / 1048576).toFixed(1)} MB`;
-    }
   }
+}
+
+function returnFileSize(number) {
+  if (number < 1024) {
+    return `${number} bytes`;
+  } else if (number >= 1024 && number < 1048576) {
+    return `${(number / 1024).toFixed(1)} KB`;
+  } else if (number >= 1048576) {
+    return `${(number / 1048576).toFixed(1)} MB`;
+  }
+}
