345e4a19ce598ac75bf443adf9227d0d1c4f1e1d
BenGrif2323 <83267311+BenGrif2323@users.noreply.github.com>
2023-03-28 12:03:06 -0700

    Added UI Elements

--- WebUI/scripts/script.js
+++ WebUI/scripts/script.js
@@ -12,41 +12,18 @@ document.querySelector('input').addEventListener('change', function () {
     ldrContent = ldrContent.split(/\r?\n/);
     document.querySelector('.output').textContent = '';
     print(ldrContent, preview);
+    ldrContent = readLDR(ldrContent);
 
   }
 
   fr.readAsText(this.files[0]);
 })
 
-function readLDR() {
-  while (preview.firstChild) {
-    preview.removeChild(preview.firstChild);
-  }
-
-  const curFiles = input.files;
-  if (curFiles.length === 0) {
-    const para = document.createElement('p');
-    para.textContent = 'No files currently selected';
-    preview.appendChild(para);
-  }
-  else if (curFiles.length > 1) {
-    const para = document.createElement('p');
-    para.textContent = 'More than one file selected';
-    preview.appendChild(para);
-  }
-  else {
-    const list = document.createElement('ul');
-    preview.appendChild(list);
-    const file = curFiles.item(0);
-    let fileReader = new FileReader();
-    fileReader.readAsText(file);
-    const listItem = document.createElement('li');
-    const para = document.createElement('li');
-    para.textContent = fileReader.result;
-    list.appendChild(para);
-    list.appendChild(listItem);
-
-  }
+function readLDR(ldrContent) {
+  const legos = [];
+  ldrContent.forEach(element => {
+    
+  })
 }
 
 function print(output, printLocation) {
